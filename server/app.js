21d469b9edb5360dbf33389f04fd308ff0e16af1
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2021-05-23 22:29:04 +0800

    Initial build

---
+++ server/app.js
@@ -0,0 +1,83 @@
+//main backend file
+//required dependencies
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express() //instance of express app
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/epicchat', {useNewUrlParser: true})
+const server = require('http').Server(app) //websocket has http server dependency
+const io = require('socket.io')(server)
+
+mongoose.Promise = global.Promise
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../dist')))
+
+//use Websockets
+io.on('connection', (socket) => {
+  let user = '';
+
+  socket.on('new message', (data) => {
+    const newMessage = new Message({
+      _id: mongoose.Types.ObjectId(),
+      message: data,
+      user: user
+    })
+    newMessage.save().then(rec => {
+      if(rec) {
+        io.emit('message recieved', rec)
+      } else {
+      }
+    })
+  })
+  socket.on('new user', (data) => {
+    user = data;
+    console.log(`${user} is connected`) //send to terminal if a new user is connected
+    socket.broadcast.emit('user connected', data); //sends event to everyone except to sender
+    Message.find().then(rec => {
+      if(rec) {
+        socket.emit('all messages', rec)
+      } else {
+      }
+    })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user disconnected', user);
+    console.log(`${user} disconnected`)
+  })
+})
+
+const Message = require('./models/message') //to use the schema in the APIs
+
+//API to get all the chat. uses HTTP protocol
+app.get('/api/chat', (req, res) => {
+  Message.find().then(rec => { //send to the browser any record found from DB
+    if(rec) {
+      res.send(rec)
+    } else {
+      res.send([]) // else send an empty array
+    }
+  })
+})
+
+//API to POST the chats to the database
+app.post('/api/chat', (req, res) => {
+  const newMessage = new Message({
+    _id: mongoose.Types.ObjectId(),
+    message: req.body.message,
+    user: 'user' //default user
+  })
+  newMessage.save().then(rec => {
+    if(rec) {
+      res.send(rec)
+    } else {
+      res.send([])
+    }
+  })
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
+
+server.listen(3000, () => console.log('Listening on port 3000...'))
---
+++ src/app/message.service.ts
@@ -0,0 +1,48 @@
+//websocket service
+
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//used by the constructor in 'app.component.ts'
+export class MessageService {
+  _chats = [];
+  _chatssub;
+  socket;
+  constructor() {
+    this._chatssub = new Subject<any[]>();
+    this.socket = io.connect();
+    this.socket.on('connect', () => {
+      console.log('connected to the server');
+    });
+    this.socket.on('message recieved', (data) => {
+      this._chats.push(data);
+      this._chatssub.next([...this._chats]);
+    });
+    this.socket.on('all messages', (data) => {
+      this._chats = [...data];
+      this._chatssub.next([...this._chats]);
+    });
+    this.socket.on('user connected', (data) => {
+      this._chats.push({message: `${data} joined the chat`, type: 'notify'});
+      this._chatssub.next([...this._chats]);
+    });
+    this.socket.on('user disconnected', (data) => {
+      this._chats.push({message: `${data} left the chat`, type: 'notify'});
+      this._chatssub.next([...this._chats]);
+    });
+  }
+  addChat(message) {
+    this.socket.emit('new message', message);
+  }
+  addUser(user) {
+    this.socket.emit('new user', user);
+  }
+  getChats() {
+    return this._chatssub.asObservable();
+  }
+}
